b0bf3fcadf157935ef1df195a2e30daf7d9c75a0
DevNikPanch <sibiryakov_7@mail.ru>
2026-09-13 11:41:53 +0400

    Working with useRef and adding autofocus to the new task input field

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,7 +1,14 @@
 import TodoItem from './TodoItem';
 
 const TodoList = (props) => {
-    const { tasks = [], filteredTasks, onDeleteTaskButtonClick, onTaskCompleteChange } = props;
+    const {
+        tasks = [],
+        filteredTasks,
+        firstIncompleteTaskRef,
+        firstIncompleteTaskId,
+        onDeleteTaskButtonClick,
+        onTaskCompleteChange,
+    } = props;
 
     const hasTasks = tasks.length > 0;
     const isEmptyFilteredTasks = filteredTasks?.length === 0;
@@ -20,6 +27,7 @@ const TodoList = (props) => {
                 <TodoItem
                     className="todo__item"
                     key={task.id}
+                    ref={task.id === firstIncompleteTaskId ? firstIncompleteTaskRef : null}
                     onDeleteTaskButtonClick={onDeleteTaskButtonClick}
                     onTaskCompleteChange={onTaskCompleteChange}
                     {...task}
